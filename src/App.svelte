35c48e23fec0663f0945b8b585e98fb1f9a00ef1
Þorsteinn <thorsteinn@star-oddi.com>
2025-01-16 11:03:27 +0000

    gröf virka rétt

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import Chart from "./lib/Chart.svelte";
 	let studull1 = 1.95
-	let studull2 = 1.92
+	let studull2 = 1.95
 	let wager1 = 25
 	let wager2 = 25
 	let winnings1, winnings2
@@ -14,15 +14,15 @@
 
 </script>
 
-<main class='w-full'>
+<main class='w-full flex flex-col p-10 h-full'>
 	<h1 class="">Bet Bestun</h1>
 	<div class='flex justify-center mt-12'>
 	<table>
 		<tbody>
 			<tr>
 				<td></td>
-				<td>Epic Stuðull</td>
-				<td>Cool Stuðull</td>
+				<td class='font-bold'>Epic Stuðull</td>
+				<td class='font-bold'>Cool Stuðull</td>
 			</tr>
 			<tr>
 				<td></td>
@@ -32,12 +32,10 @@
 				<td>
 					<input type="number" class='w-32 text-center' step="0.01" bind:value={studull2}>
 				</td>
-				<td>
-					Samtals Upphæð
-				</td>
+				<td class='font-bold'>Samtals Upphæð</td>
 			</tr>
 			<tr class='border-b border-white'>
-				<td>Upphæð</td>
+				<td class='font-bold'>Upphæð</td>
 				<td>
 					<input type="number" class='w-32 text-center' step="1" bind:value={wager1}>
 				</td>
@@ -47,19 +45,22 @@
 				<td>{wager1 + wager2}</td>
 			</tr>
 			<tr>
-				<td>Vinningur</td>
+				<td class='font-bold'>Vinningur</td>
 				<td>{winnings1}</td>
 				<td>{winnings2}</td>
 			</tr>
 			<tr>
-				<td>Tap</td>
+				<td class='font-bold'>Tap</td>
 				<td>{loss1}</td>
 				<td>{loss2}</td>
 			</tr>
 		</tbody>
 	</table>
 	</div>
-	<Chart title={`Epicbet wager = X,  Coobet wager = ${wager2}`} bind:studull1 bind:studull2 bind:wager={wager1}/>
+	<div class='flex w-full h-full mt-10'>
+		<Chart title={`Epicbet wager = X,  Coolbet wager = ${wager2}`} bind:studull1 bind:studull2 bind:wager={wager2} fixedWagerSite='Coolbet' variableWagerSite='Epicbet'/>
+		<Chart title={`Coolbet wager = X,  Epicbet wager = ${wager1}`} bind:studull1={studull2} bind:studull2={studull1} bind:wager={wager1} fixedWagerSite='Epicbet' variableWagerSite='Coolbet'/>
+	</div>
 </main>
 
 <style>
--- src/lib/Chart.svelte
+++ src/lib/Chart.svelte
@@ -6,80 +6,120 @@
 	export let studull1:number;
 	export let studull2:number;
 	export let wager:number;
+	export let fixedWagerSite:string
+	export let variableWagerSite:string;
 
 	let labels:number[] = []
 	let data1:number[] = [];
 	let data2:number[] = [];
 
-	let chart;
+	let canvas:HTMLCanvasElement;
+	let chart:Chart;
+
+	$: onChange(studull1, studull2, wager)
+
+	function onChange(...args){
+		if(chart){
+			chart.data = generateData()
+			chart.update();
+		}
+	}
 
 	function generateData(){
 		data1 = []
 		data2 = []
 		labels = []
-		let x, loss1, loss2, win2
-		let win1 = wager*studull1
-		for(let i = 0; i < 2*wager; i += 0.5){
-			labels.push(i)
-			win2 = i*studull2;
-			loss1 = (wager + i) - win1
-			loss2 = (wager + i) - win2
+		let loss1, loss2, win1, wagerSum
+		let win2 = wager*studull2
+		for(let i = 0.5*wager; i < 1.5*wager; i += 0.05){
+			labels.push(Math.round(i*100)/100)
+			win1 = i*studull1;
+			wagerSum = wager + i
+			loss1 = Math.round((wagerSum - win1)*100)/100
+			loss2 = Math.round((wagerSum - win2)*100)/100
+			//data1.push(Math.max(loss1, loss2))
+			//data2.push(Math.min(loss1,loss2))
 			data1.push(loss1)
 			data2.push(loss2)
 		}
+
 		return {
 			labels: labels,
-			datasets: [{
-				label: 'Tap',
-				data: data1,
-				fill: false,
-				borderColor: 'rgb(75, 192, 192)',
-				tension: 0.1
-			},
-			{
-				label: 'Tap',
-				data: data2,
-				fill: false,
-				borderColor: 'rgb(192, 20, 20)',
-				tension: 0.1
-			}]
+			datasets: [
+				{
+					label: `Tap ef ${variableWagerSite} vinnur`,
+					data: data1,
+					fill: false,
+					borderColor: 'rgb(75, 192, 192)',
+					backgroundColor: 'rgb(75, 192, 192)',
+					pointRadius: 1,
+					pointStyle: 'circle'
+				},
+				{
+					label: `Tap ef ${fixedWagerSite} vinnur`,
+					data: data2,
+					fill: false,
+					borderColor: 'rgb(192, 20, 20)',
+					backgroundColor: 'rgb(192, 20, 20)',
+					pointRadius: 1,
+					pointStyle: 'circle'
+				},
+			]
 		}
 	}
 
 	onMount(() => {
-		var ctx = document.getElementById('canvas').getContext('2d');
-
-		chart = new Chart(ctx, 
-			{
-				type: 'line',
-				options: {
-					animation: false,
-					plugins: {
-						legend: {
-							display: false
+		var ctx = canvas.getContext('2d');
+		if(ctx) {
+			chart = new Chart(ctx, 
+				{
+					type: 'line',
+					options: {
+						responsive: true,
+						maintainAspectRatio: false,
+						animation: false,
+						scales: {
+							y: {
+								grid: {
+									lineWidth: ({ tick }) => tick.value == 0 ? 2 : 1,
+									color: ({ tick }) => tick.value == 0 ? 'rgb(255, 255, 255)' : 'rgba(20,20,20, 0.3)',
+								}
+							}
 						},
-						tooltip: {
-							enabled: true
+						plugins: {
+							legend: {
+								labels: {
+									color: 'white',
+									usePointStyle: true,
+									pointStyle: 'line',
+									pointStyleWidth: 50
+								}
+							},
+							tooltip: {
+								intersect: false,
+								mode: 'index',
+								usePointStyle: true,
+							}
+						},
+						elements: {
+							point: {
+								pointStyle: false
+							}
 						}
 					},
-					elements: {
-						point: {
-							pointStyle: false
-						}
-					}
-				},
-				data: generateData()
-			}
-		);
+					data: generateData()
+				}
+			);
+		}
 	})
 
 
 	
 </script>
 
-<div>
+<div class='w-full'>
 	<h2>{title}</h2>
 	<div>
-		<canvas id='canvas' height="200" width="300"></canvas>
+		<canvas bind:this={canvas} height="500"></canvas>
 	</div>
 </div>
